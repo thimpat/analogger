87f8b8d2373bbf24b09fa0bf734a8c405739f221
Patrice THIMOTHEE <thimpat@gmail.com>
2022-02-08 23:01:49 +0000

    TEST: Fix tests

--- test/integration.cjs
+++ test/integration.cjs
@@ -38,16 +38,6 @@ describe('In the Terminal', function ()
             expect(captured.stdout).to.contain(`Test Log example C1`)
         });
 
-        it('should display some text with colours', function (done)
-        {
-            const captured = capcon.captureStdio(function ()
-            {
-                anaLogger.log(LOG_CONTEXT.C1, `Test Log example C1`);
-                expect(anaLogger.getLogHistory()).to.contain(`[38;2;255;228;181m`)
-                done()
-            })
-        });
-
         it('should hide console output when the console behaviour is overridden', function ()
         {
             const captured = capcon.captureStdio(function ()
