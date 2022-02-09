8668c46cac2dd8f0bdc8be0b5fab166397430299
Patrice THIMOTHEE <thimpat@gmail.com>
2022-02-09 02:21:53 +0000

    DEMO: Update examples

--- example/cjs/demo.cjs
+++ example/cjs/demo.cjs
@@ -1,18 +1,18 @@
-const {LOG_CONTEXT, LOG_TARGETS} = require("./contexts-def.cjs")
+const {LOG_CONTEXTS, LOG_TARGETS} = require("./contexts-def.cjs")
 // const {anaLogger} = require("../../dist/index-cjs.min.cjs");
 const {anaLogger} = require("../../src/cjs/ana-logger.cjs");
 
 anaLogger.keepLogHistory()
 
-anaLogger.setContexts(LOG_CONTEXT);
+anaLogger.setContexts(LOG_CONTEXTS);
 anaLogger.setTargets(LOG_TARGETS);
 anaLogger.setActiveTarget(LOG_TARGETS.DEV3)
 anaLogger.setOptions({silent: true})
 
 console.log("==========================");
-anaLogger.log(LOG_CONTEXT.C1, `Test Log example C1`);
-anaLogger.log(LOG_CONTEXT.C2, `Test Log example C2`);
-anaLogger.log(LOG_CONTEXT.C3, `Test Log example C3`);
+anaLogger.log(LOG_CONTEXTS.C1, `Test Log example C1`);
+anaLogger.log(LOG_CONTEXTS.C2, `Test Log example C2`);
+anaLogger.log(LOG_CONTEXTS.C3, `Test Log example C3`);
 
 anaLogger.setOptions({silent: false, hideError: false})
 console.log(anaLogger.getLogHistory())
@@ -31,29 +31,29 @@ anaLogger.overrideError()
 console.error(`Error After override`);
 console.log("==========================");
 
-anaLogger.log(LOG_CONTEXT.STANDARD, `Basic Log example 2`, "+Something 0", "+Something 1");
+anaLogger.log(LOG_CONTEXTS.STANDARD, `Basic Log example 2`, "+Something 0", "+Something 1");
 
-anaLogger.log({context: LOG_CONTEXT.TEST, lid: 100000}, `Test Log example`);
-anaLogger.log({context: LOG_CONTEXT.TEST, target: LOG_TARGETS.DEV3, lid: 100001}, `Test Log example with active target`);
-anaLogger.log({context: LOG_CONTEXT.TEST, target: LOG_TARGETS.DEV1, lid: 100002}, `Test Log example with DEV1 target`);
-anaLogger.log({context: LOG_CONTEXT.TEST, target: LOG_TARGETS.DEV2, lid: 100003}, `Test Log example with DEV2 target`);
+anaLogger.log({context: LOG_CONTEXTS.TEST, lid: 100000}, `Test Log example`);
+anaLogger.log({context: LOG_CONTEXTS.TEST, target: LOG_TARGETS.DEV3, lid: 100001}, `Test Log example with active target`);
+anaLogger.log({context: LOG_CONTEXTS.TEST, target: LOG_TARGETS.DEV1, lid: 100002}, `Test Log example with DEV1 target`);
+anaLogger.log({context: LOG_CONTEXTS.TEST, target: LOG_TARGETS.DEV2, lid: 100003}, `Test Log example with DEV2 target`);
 anaLogger.log(`Test Log example with DEFAULT target`);
 
-anaLogger.log(LOG_CONTEXT.TEST, `Test Log example`, "+Something 3");
-anaLogger.log(LOG_CONTEXT.C1, `Test Log example C1`);
-anaLogger.log(LOG_CONTEXT.C2, `Test Log example C2`);
-anaLogger.log(LOG_CONTEXT.C3, `Test Log example C3`);
+anaLogger.log(LOG_CONTEXTS.TEST, `Test Log example`, "+Something 3");
+anaLogger.log(LOG_CONTEXTS.C1, `Test Log example C1`);
+anaLogger.log(LOG_CONTEXTS.C2, `Test Log example C2`);
+anaLogger.log(LOG_CONTEXTS.C3, `Test Log example C3`);
 
-console.log(LOG_CONTEXT.C1, `Test Log example C4`);
-console.log(LOG_CONTEXT.C1, `Test Log example C5`);
-console.log(LOG_CONTEXT.C1, `Test Log example C6`);
+console.log(LOG_CONTEXTS.C1, `Test Log example C4`);
+console.log(LOG_CONTEXTS.C1, `Test Log example C5`);
+console.log(LOG_CONTEXTS.C1, `Test Log example C6`);
 
-console.error({context: LOG_CONTEXT.ERROR, lid: 200000}, `Testing Error 1`)
-console.error(LOG_CONTEXT.ERROR, `Testing Error 2`)
+console.error({context: LOG_CONTEXTS.ERROR, lid: 200000}, `Testing Error 1`)
+console.error(LOG_CONTEXTS.ERROR, `Testing Error 2`)
 console.error(`Testing Error 3`)
 console.error(undefined, `Testing Error 4`)
 
-console.error({context: LOG_CONTEXT.ERROR, target: LOG_TARGETS.USER, lid: 200010}, `Testing Error 4`)
+console.error({context: LOG_CONTEXTS.ERROR, target: LOG_TARGETS.USER, lid: 200010}, `Testing Error 4`)
 
 anaLogger.alert(`Hello from alert`, {aaa: 1012})
 
@@ -65,7 +65,7 @@ anaLogger.setErrorHandlerForUserTarget(function (context/*, ...args*/)
     }
 })
 
-console.error({context: LOG_CONTEXT.ERROR, target: LOG_TARGETS.USER, lid: 200020}, `Testing Error that triggers a special handler`)
+console.error({context: LOG_CONTEXTS.ERROR, target: LOG_TARGETS.USER, lid: 200020}, `Testing Error that triggers a special handler`)
 
 anaLogger.setLogFormat(
     function({contextName, message})
@@ -74,8 +74,8 @@ anaLogger.setLogFormat(
     }
 );
 
-console.log(LOG_CONTEXT.C1, `Test Log example C4 with new format`);
-console.log(LOG_CONTEXT.C1, `Test Log example C5 with new format`);
-console.log(LOG_CONTEXT.C1, `Test Log example C6 with new format`);
+console.log(LOG_CONTEXTS.C1, `Test Log example C4 with new format`);
+console.log(LOG_CONTEXTS.C1, `Test Log example C5 with new format`);
+console.log(LOG_CONTEXTS.C1, `Test Log example C6 with new format`);
 
 anaLogger.log(`Basic Log example 1`);
