9bd6a2d8fafa5dbad41353e6687f56ee04aee232
Patrice THIMOTHEE <thimpat@gmail.com>
2022-02-10 03:49:10 +0000

    TEST: Setup E2E

--- test/e2e.cjs
+++ test/e2e.cjs
@@ -4,31 +4,38 @@ const chrome = require("selenium-webdriver/chrome");
 const chromium = require("chromium");
 require("chromedriver");
 
-async function start() {
-    let options = new chrome.Options();
-    options.setChromeBinaryPath(chromium.path);
-    options.addArguments("--headless");
-    options.addArguments("--sandbox");
-    options.addArguments("--disable-gpu");
-    options.addArguments("--window-size=1280,960");
+async function init()
+ {
+     let options = new chrome.Options();
+     options.setChromeBinaryPath(chromium.path);
+     options.addArguments("--headless");
+     options.addArguments("--sandbox");
+     options.addArguments("--disable-gpu");
+     options.addArguments("--window-size=1280,960");
 
-    const driver = await new webdriver.Builder()
-        .forBrowser("chrome")
-        .setChromeOptions(options)
+     const driver = await new webdriver.Builder()
+         .forBrowser("chrome")
+         .setChromeOptions(options)
         .build();
 
-    await driver.get("http://google.com");
-    console.log("Hello Google!");
-    await takeScreenshot(driver, "google-start-page");
+    await driver.get("http://127.0.0.1:9876/index.html");
+    await takeScreenshot(driver, "test");
 
-    await driver.quit();
+    return driver
 }
 
-async function takeScreenshot(driver, name) {
-    await driver.takeScreenshot().then((data) => {
+async function takeScreenshot(driver, name)
+{
+    await driver.takeScreenshot().then((data) =>
+    {
         fs.writeFileSync(name + ".png", data, "base64");
-        console.log("Screenshot is saved");
     });
 }
 
-start();
+(async function ()
+{
+    const driver = await init();
+
+    await driver.quit();
+}())
+
