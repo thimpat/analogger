95207761fee49c0f48f56b469ca2e4f30a909b9d
Patrice THIMOTHEE <thimpat@gmail.com>
2022-02-11 08:54:22 +0000

    TEST: Add tests against added code

--- test/2-integration-filesystem.cjs
+++ test/2-integration-filesystem.cjs
@@ -1,114 +1,131 @@
 const chai = require("chai");
-var capcon = require('capture-console');
+var capcon = require("capture-console");
 const {anaLogger} = require("../src/cjs/ana-logger.cjs");
 const {LOG_CONTEXTS, LOG_TARGETS} = require("../example/cjs/contexts-def.cjs");
+const fs = require("fs");
 const expect = chai.expect;
 
-describe('In the Terminal', function ()
+describe("In the Terminal", function ()
 {
     before(()=>
     {
         anaLogger.setContexts(LOG_CONTEXTS);
         anaLogger.setTargets(LOG_TARGETS);
-    })
+    });
+
+    after(()=>
+    {
+        if (fs.existsSync("./test-log.log"))
+        {
+            fs.unlinkSync("./test-log.log");
+        }
+    });
 
     beforeEach(()=>
     {
-        anaLogger.resetLogHistory()
-        anaLogger.keepLogHistory()
-        anaLogger.setOptions({silent: false, hideError: false})
-        anaLogger.removeOverride()
-        anaLogger.removeOverrideError()
-    })
+        anaLogger.resetLogHistory();
+        anaLogger.keepLogHistory();
+        anaLogger.setOptions({silent: false, hideError: false});
+        anaLogger.removeOverride();
+        anaLogger.removeOverrideError();
+    });
 
-    describe('AnaLogger', function ()
+    describe("AnaLogger", function ()
     {
-        it('should emulate console.log', function ()
+        it("should emulate console.log", function ()
         {
             const captured = capcon.captureStdio(function ()
             {
-                anaLogger.log(`Test Log example C1`);
-            })
+                anaLogger.log("Test Log example C1");
+            });
 
-            expect(captured.stdout).to.contain(`Test Log example C1`)
+            expect(captured.stdout).to.contain("Test Log example C1");
         });
 
-        it('should hide console output when the console behaviour is overridden', function (done)
+        it("should hide console output when the console behaviour is overridden", function (done)
         {
             const captured = capcon.captureStdio(function ()
             {
-                console.log(`Log Before override`);
-                anaLogger.setOptions({silent: true})
-                anaLogger.overrideConsole()
-                console.log(`Log After override`);
-                done()
-            })
+                console.log("Log Before override");
+                anaLogger.setOptions({silent: true});
+                anaLogger.overrideConsole();
+                console.log("Log After override");
+                done();
+            });
 
-            expect(captured.stdout).to.contain(`Log Before override`)
-            expect(captured.stdout).to.not.contain(`Log After override`)
+            expect(captured.stdout).to.contain("Log Before override");
+            expect(captured.stdout).to.not.contain("Log After override");
         });
 
-        it('should hide console output but keep console input in the log history', function ()
+        it("should hide console output but keep console input in the log history", function ()
         {
             const captured1 = capcon.captureStdio(function ()
             {
                 anaLogger.keepLogHistory();
-                anaLogger.setOptions({silent: true, hideError: false})
-                anaLogger.log(LOG_CONTEXTS.C1, `Test Log example something again`);
-            })
+                anaLogger.setOptions({silent: true, hideError: false});
+                anaLogger.log(LOG_CONTEXTS.C1, "Test Log example something again");
+            });
 
             const captured2 = capcon.captureStdio(function ()
             {
-                console.log(anaLogger.getLogHistory())
-            })
+                console.log(anaLogger.getLogHistory());
+            });
 
-            expect(captured1.stdout).to.not.contain(`Test Log example something again`)
-            expect(captured2.stdout).to.contain(`Test Log example something again`)
+            expect(captured1.stdout).to.not.contain("Test Log example something again");
+            expect(captured2.stdout).to.contain("Test Log example something again");
         });
 
-        it('should hide console error when the console behaviour is overridden', function ()
+        it("should hide console error when the console behaviour is overridden", function ()
         {
             const captured = capcon.captureStdio(function ()
             {
-                console.log(`Log Before override`);
-                anaLogger.setOptions({hideError: true})
-                anaLogger.overrideConsole()
+                console.log("Log Before override");
+                anaLogger.setOptions({hideError: true});
+                anaLogger.overrideConsole();
 
-                console.error(`Error Before override`);
-                anaLogger.overrideError()
-                console.error(`Error After override`);
+                console.error("Error Before override");
+                anaLogger.overrideError();
+                console.error("Error After override");
 
-                console.log(`Log After override`);
-            })
+                console.log("Log After override");
+            });
 
-            expect(captured.stdout).to.contain(`Log Before override`)
-            expect(captured.stdout).to.not.contain(`Error After override`)
+            expect(captured.stdout).to.contain("Log Before override");
+            expect(captured.stdout).to.not.contain("Error After override");
         });
 
-        it('should not show unrelated target logs', function ()
+        it("should not show unrelated target logs", function ()
         {
             const captured = capcon.captureStdio(function ()
             {
-                anaLogger.setActiveTarget(LOG_TARGETS.DEV3)
-                anaLogger.log({context: LOG_CONTEXTS.TEST, target: LOG_TARGETS.DEV3, lid: 100001}, `Test Log example with active target`);
-                anaLogger.log({context: LOG_CONTEXTS.TEST, target: LOG_TARGETS.DEV1, lid: 100002}, `Test Log example with DEV1 target`);
-                anaLogger.log(`Test Log example with DEFAULT target`);
-            })
+                anaLogger.setActiveTarget(LOG_TARGETS.DEV3);
+                anaLogger.log({context: LOG_CONTEXTS.TEST, target: LOG_TARGETS.DEV3, lid: 100001}, "Test Log example with active target");
+                anaLogger.log({context: LOG_CONTEXTS.TEST, target: LOG_TARGETS.DEV1, lid: 100002}, "Test Log example with DEV1 target");
+                anaLogger.log("Test Log example with DEFAULT target");
+            });
 
-            expect(captured.stdout).to.contain(`Test Log example with active target`)
-            expect(captured.stdout).to.contain(`Test Log example with DEFAULT target`)
-            expect(captured.stdout).to.not.contain(`Test Log example with DEV1 target`)
+            expect(captured.stdout).to.contain("Test Log example with active target");
+            expect(captured.stdout).to.contain("Test Log example with DEFAULT target");
+            expect(captured.stdout).to.not.contain("Test Log example with DEV1 target");
         });
 
-        it('should not anything when silent mode is enabled', function ()
+        it("should not do anything when silent mode is enabled", function ()
         {
             const captured = capcon.captureStdio(function ()
             {
-                anaLogger.setOptions({silent: true})
-                anaLogger.log(`Test Log example with DEFAULT target`);
-            })
+                anaLogger.setOptions({silent: true});
+                anaLogger.log("Test Log example with DEFAULT target");
+            });
 
-            expect(captured.stdout).to.not.contain(`Test Log example with DEFAULT target`)
+            expect(captured.stdout).to.not.contain("Test Log example with DEFAULT target");
+        });
+
+        it("should write logs to a file when logToFile is on", function ()
+        {
+            anaLogger.setOptions({silent: true, logToFile: "./test-log.log"});
+            anaLogger.log("Test Log example with DEFAULT target");
+            const content = fs.readFileSync("./test-log.log", "utf-8");
+            expect(content).to.contain("Test Log example with DEFAULT target");
         });
 
 
