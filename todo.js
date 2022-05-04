1c4c85886664f25eddc8a79eb91efc1f7983df16
Patrice Thimothee <thimpat@gmail.com>
2022-05-04 08:40:44 +0100

    PROD: Update task list

--- todo.js
+++ todo.js
@@ -1,2 +1,12 @@
-// @todo Log to server
-// @todo Fix setOptions
+/** @todo Log to server
+ * ----------------------------------------------------------------------
+ * - genserve start analogger --port 3036 --namespace analogger
+ * - Set an URL to analogger (http://localhost:3036/analogger.server.js)
+ * - analogger.server.js should be able to forward all of the logs to a file
+ * => Have an NPM, that launches genserve?
+ * => Make analogger do the remote work as well?: analogger-server
+ * ----------------------------------------------------------------------
+ *
+ * @todo Fix setOptions
+ *
+ */
