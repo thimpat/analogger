176cadb624df162abb4f8de494dab8dbb1bec27e
Patrice Thimothee <thimpat@gmail.com>
2022-06-06 03:02:09 +0100

    TEST: Update tests

--- test/lib/test-utils.cjs
+++ test/lib/test-utils.cjs
@@ -28,3 +28,43 @@ module.exports.waitForDriverCaptured = (monitor) =>
     });
 };
 
+/**
+ * Returns file content
+ * @param filepath
+ * @returns {string|any}
+ */
+module.exports.getContent = (filepath) =>
+{
+    try
+    {
+        const str = fs.readFileSync(filepath, "utf-8");
+        try
+        {
+            return JSON.parse(str);
+        }
+        catch (e)
+        {
+
+        }
+        return str;
+    }
+    catch (e)
+    {
+        console.error("E56446556231131: ", e);
+    }
+};
+
+/**
+ * Delay code execution for a number of milliseconds
+ * @param {number} ms Number of milliseconds to delay the code
+ * @returns {Promise<unknown>}
+ */
+module.exports.sleep =
+function sleep(ms)
+{
+    return new Promise((resolve) =>
+    {
+        setTimeout(resolve, ms);
+    });
+};
+
