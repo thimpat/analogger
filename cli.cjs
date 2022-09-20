b8072e47b73cfe6ac60d2f7b37b828f2f85c00cd
Patrice Thimothee <thimpat@gmail.com>
2022-09-20 20:31:09 +0100

    FEAT: Launch a remote from AnaLogger

--- cli.cjs
+++ cli.cjs
@@ -1,50 +1,15 @@
 #!/usr/bin/env node
 let packageJson = require("./package.json");
 
-const {spawn} = require("child_process");
-const os = require("os");
-
 const { program } = require("commander");
+const {openRemoteLogging} = require("./helpers/helpers.cjs");
 
-const openRemoteLogging = (argv) =>
-{
-    const args = argv.slice(2);
-
-    const npmPath = os.platform() === "win32" ? "npm.cmd" : "npm";
-
-    const npx = spawn(npmPath, ["exec", "remote-logging", ...args], {
-        stdio      : ["pipe", "pipe", "pipe"],
-        windowsHide: true,
-        shell: false
-    });
-
-    npx.stdout.on("data", (data) =>
-    {
-        console.log(`stdout: ${data}`);
-        // Workaround
-        process.exit(0);
-    });
-
-    npx.stderr.on("data", (data) =>
-    {
-        console.log(`stderr: ${data}`);
-    });
-
-    npx.on("close", (code) =>
-    {
-        console.log(`child process exited with code ${code}`);
-    });
-
-    npx.unref();
-
-};
 
 program
     .name(packageJson.name)
     .description("Open a remote server for AnaLogger")
     .version(packageJson.version, "-v, --version", "output the AnaLogger current version")
-    .option("-p, --port", "Remote Logging port to use", 12000)
-    .option("-s, --server", "Open Remote Logging GUI")
+    .option("-p, --port <number>", "Remote Logging port to use", 12000)
     .showHelpAfterError("(add --help for additional information)")
     .showSuggestionAfterError(true);
 
@@ -54,9 +19,7 @@ program
     program.parse();
 
     const options = program.opts();
-
     const args = process.argv.slice(2);
-
     openRemoteLogging(args, options);
     
 }());
