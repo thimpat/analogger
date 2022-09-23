cfa9a07f9f435708e277863b1cfd3531ac09dc77
Patrice Thimothee <thimpat@gmail.com>
2022-09-24 00:33:48 +0100

    CI: Update launchers

--- .toesm.cjs
+++ .toesm.cjs
@@ -1,41 +1,14 @@
-const os = require("os");
 module.exports = {
-    replaceStart: [
-        {
-            search : "const colorConvert = require(\"color-convert-cjs\");",
-            replace: `const colorConvert = null;`
-        },
-        {
-            search : /const\s+chalk\s*=\s*require\(.chalk-cjs.\)[;]?/g,
-            replace: `${os.EOL}const chalk = null;${os.EOL}`
-        },
-    ],
-    replaceEnd  : [
-        {
-            search : `// ***`,
-            replace: "// ---------"
-        }
-    ],
     replaceModules:
         {
             "node-fetch": {
                 cjs: {
-                    name: "node-fetch-cjs",
+                    name   : "node-fetch-cjs",
                     version: "@2.6.7"
                 },
                 esm: {
                     version: "@3.2.10"
                 }
-            },
-            "term-size": {
-                cjs: {
-                    name: "term-size-cjs",
-                    version: "@2.2.1"
-                },
-                esm: {
-                    version: "@3.0.2"
-                }
-            },
+            }
         }
-
 }
