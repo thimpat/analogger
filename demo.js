6f8978a64fd9c68cac5b37c7066088c2afd9161c
Patrice Thimothee <thimpat@gmail.com>
2025-02-26 10:10:19 +0000

    demo: Update demo

--- demo.js
+++ demo.js
@@ -2,7 +2,13 @@ const AnaLogger = require("./");
 
 const anaLogger = new AnaLogger();
 
-anaLogger.setOptions({silent: false, lidLenMax: 8, logToRemote: true});
+anaLogger.setOptions({
+    silent: false, lidLenMax: 8, logToRemote: true,
+    logMaxSize: 1000,
+    logMaxArchives: 5,
+    logToFile: "./logs/demo.log"
+});
+
 anaLogger.setDefaultContext({
     "contextName": "DEFAULT",
     "target": "USER",
@@ -17,3 +23,11 @@ anaLogger.setActiveTargets("localhost, api");
 anaLogger.log({lid: "WEB35382", target: "localhost", color: "yellow"}, `Log with target`);
 anaLogger.log({lid: "WEB35382", target: "localhost"}, `Log with target`);
 anaLogger.log({lid: "WEB35382", color: "green", target: "localhost"}, `Log without target`);
+
+// Check archive
+for (let i = 1; i < 100; ++i) {
+    anaLogger.log({
+        lid: "WEB35382",
+        target: "localhost"
+    }, `${i}: Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.`);
+}
