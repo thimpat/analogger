26fa8e181a8427bdda529a8c63aee8b6e1f71b54
Patrice Thimothee <thimpat@gmail.com>
2025-03-26 18:03:46 +0000

    demo: Enhance logging examples with additional context and targets

--- demo.cjs
+++ demo.cjs
@@ -11,6 +11,11 @@ anaLogger.setOptions({
     compressArchives: true,
 });
 
+anaLogger.setContext("TEST", { color: "#5d8a6b", symbol: "email", name: "TEST", lid: "12222" });
+anaLogger.log({lid: "WEB35382", color: "yellow"}, `Log with target`);
+anaLogger.log({contextName: "TEST"}, `Test Log example TEST`);
+anaLogger.log({contextName: "TEST"}, `Test Log example TEST 2`);
+
 anaLogger.setDefaultContext({
     "contextName": "DEFAULT",
     "target": "USER",
@@ -22,7 +27,7 @@ anaLogger.setDefaultContext({
     onContext: function (context) {
         context.lid = "MOD12345";
     },
-    onMessage: function (rawMessage, extras) {
+    onMessage: function (rawMessage/**, extras **/) {
         return rawMessage + " ↘";
     },
     onOutput: function (formattedMessage, {logCounter}) {
@@ -30,13 +35,16 @@ anaLogger.setDefaultContext({
         return `${logCounter}: ${formattedMessage}`;
     }
 });
+anaLogger.log({lid: "WEB35382", color: "yellow", target: "DEFAULT"}, `Log with target 1`);
+anaLogger.log({lid: "WEB35384", color: "yellow", target: "USER"}, `Log with non-existent target 2`);
+anaLogger.log({lid: "WEB35386", color: "yellow", target: undefined}, `Log with undefined target 3`);
+anaLogger.log({lid: "WEB35388", color: "yellow"}, `Log without target 3`);
 
 anaLogger.setActiveTargets("localhost, api");
 anaLogger.log({lid: "WEB35382", target: "localhost", color: "yellow"}, `Log with target`);
 anaLogger.log({lid: "WEB35382", target: "localhost"}, `Log with target`);
 anaLogger.log({lid: "WEB35382", color: "green", target: "localhost"}, `Log without target`);
 
-anaLogger.setContext("TEST", { color: "#5d8a6b", symbol: "email", name: "TEST" });
 anaLogger.log({contextName: "TEST", target: "localhost"}, `Test Log example TEST`);
 anaLogger.log({contextName: "TEST", target: "localhost"}, `Test Log example TEST 2`);
 
